b0db604c46c6643e2c9b3d03b212a6a51c1eafb6
Jargette <leane.caillerez@gmail.com>
2026-05-28 00:33:19 +0200

    Update DOMManager.js

--- js/DOMManager.js
+++ js/DOMManager.js
@@ -1,3 +1,5 @@
+import {Game} from "./Game.js";
+
 export class DOMManager {
 
     /**
@@ -7,7 +9,7 @@ export class DOMManager {
     createCards(images) {
         const gameBoard = document.querySelector('.game-board');
         // On vide le plateau pour pas que les cartes se superposent si on rejoue
-        gameBoard.innerHTML = ''; 
+        gameBoard.innerHTML = '';
 
         const template = document.querySelector('#carteAJouer');
         let tab = [];
@@ -25,25 +27,26 @@ export class DOMManager {
             newDiv.querySelector('.card-back')
                 .querySelector('img')
                 .setAttribute('alt', image.name);
-         
+
             gameBoard.append(newDiv);
             i++;
         }
     }
 
     /**
-     * Montre l'autre face de carte
+     * Retourne une carte
+     * @param {Event} event
      */
-    returnCard(event){
-       const carte=event.target.closest('.card')
+    returnCard(event) {
+        const carte = event.target.closest('.card')
         carte.classList.toggle('flip');
 
     }
 
-/**
- * Crée un bouton qui permet d'abandonner la partie
- * @param {Game} game La partie concernée par le bouton
- */
+    /**
+     * Crée un bouton qui permet d'abandonner la partie
+     * @param {Game} game La partie concernée par le bouton
+     */
     boutonAbandon(game) {
         const abandon = document.createElement('button')
         abandon.id = 'abandon'
@@ -52,17 +55,80 @@ export class DOMManager {
         document.querySelector('.game-area-header').append(abandon)
     }
 
-        /**
-         * Voici un exemple de contenu de card permettant de contenir une partie masqué
-         * et l'image qui doit être révélée.
-         *
-         <div class="card-inner">
-         <div class="card-front">
-         <img src="./assets/images/mask1.jpg" alt="Hidden card">
-         </div>
-         <div class="card-back hidden">
-         <img src="${image.url}" alt="${image.name}">
-         </div>
-         </div>
-         */    
+    /**
+     * Affiche le nombre de vies restantes
+     * @param {Game} game
+     */
+    voirVieRestante(game) {
+        debugger
+        if (Number.isInteger(game.vies)) {
+            const lives = document.createElement('div')
+            lives.id = "affichage-vies";
+            lives.classList.add('game-timer');
+            lives.innerHTML = `Vies restantes : ${game.vies}`;
+            document.querySelector('.game-area-header').append(lives);
+        }
+    }
+
+    /**
+     * Lance un chronomètre ou un temps imparti selon le mode sélectionné
+     * @param {Game} game La partie en cours
+     */
+    starTimer(game) {
+        // Choix du chronomètre ou temps limité
+        switch (game.mode) {
+            case 1:
+                this.startLimitedTimer(game);
+                break;
+            default:
+                this.startUnlimitedTimer(game);
+        }
+    }
+
+    /**
+     * Lance un temps limité pour finir le jeu
+     * @param {Game} game La partie en cours
+     */
+    startLimitedTimer(game) {
+        let time = game.cartes.length * 2; // Temps suffisant mais pas trop long
+        const timer = document.createElement('div');
+        timer.classList.add('game-timer');
+        timer.innerHTML = `Temps restant : ${time}s`;
+        document.querySelector('.game-area-header').append(timer);
+        game.timerInterval = setInterval(() => {
+            time -= 1;
+            timer.innerHTML = `Temps restant : ${time}s`;
+            if (time === 0) {
+                game.endGame();
+            }
+        }, 1000);
+    }
+
+    /**
+     * Lance un chronomètre
+     * @param {Game} game La partie en cours
+     */
+    startUnlimitedTimer(game) {
+        let time = 0;
+        const timer = document.createElement('div');
+        timer.classList.add('game-timer');
+        timer.innerHTML = `Temps écoulé : ${time}s`;
+        document.querySelector('.game-area-header').append(timer);
+        game.timerInterval = setInterval(() => {
+            time += 1;
+            timer.innerHTML = `Temps écoulé : ${time}s`;
+        }, 1000);
+    }
+
+    /**
+     * Affiche le compteur de coups en haut
+     * @param {Game} game La partie en cours
+     */
+    initCompteurCoups(game) {
+        const coupsDiv = document.createElement('div');
+        coupsDiv.id = 'affichage-coups';
+        coupsDiv.classList.add('game-timer');
+        coupsDiv.innerHTML = `Coups : ${game.nbCoups}`;
+        document.querySelector('.game-area-header').append(coupsDiv);
+    }
 }
